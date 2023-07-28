906fa0b5342cc5119b31e9464448b8935274ff02
Denis Babin <markelizarov999@gmail.com>
2023-07-29 00:01:35 +0300

    added poiarche

---
+++ POIARCHE/src/scripts/index.js
@@ -0,0 +1,31 @@
+// // Burger Menu
+
+// const burgerBtn = document.getElementById('burger-btn');
+// const burgerMenu = document.querySelector('.burger-menu');
+// const langBtn = document.querySelector('.header__lang');
+// const publishBtn = document.querySelector('.header__btn-publish');
+
+// const hideBurgerMenu = () => {
+//   document.body.style.overflow = '';
+//   burgerMenu.classList.remove('show');
+//   burgerBtn.classList.remove('active');
+//   langBtn.classList.remove('display-none');
+//   publishBtn.classList.remove('display-none');
+// }
+// const showBurgerMenu = () => {
+//   document.body.style.overflow = 'hidden';
+//   burgerMenu.classList.add('show');
+//   burgerBtn.classList.add('active');
+//   langBtn.classList.add('lang-btn--hidden');
+//   publishBtn.classList.add('display-none');
+// }
+
+// const toggleBurgerMenu = () => {
+//   if (burgerMenu.classList.contains('show')) {
+//     hideBurgerMenu()
+//   } else {
+//     showBurgerMenu()
+//   }
+// };
+
+// burgerBtn.addEventListener('click', toggleBurgerMenu);
