f02f81a6ca56cc1bd571049d106b7b28556a143f
Denis Babin <markelizarov999@gmail.com>
2023-07-29 00:11:15 +0300

    added dev

---
+++ POIARCHE/dist/assets/js/index.js
@@ -0,0 +1,49 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/scripts/index.js":
+/*!******************************!*\
+  !*** ./src/scripts/index.js ***!
+  \******************************/
+/***/ ((__unused_webpack___webpack_module__, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n// // Burger Menu\r\n\r\n// const burgerBtn = document.getElementById('burger-btn');\r\n// const burgerMenu = document.querySelector('.burger-menu');\r\n// const langBtn = document.querySelector('.header__lang');\r\n// const publishBtn = document.querySelector('.header__btn-publish');\r\n\r\n// const hideBurgerMenu = () => {\r\n//   document.body.style.overflow = '';\r\n//   burgerMenu.classList.remove('show');\r\n//   burgerBtn.classList.remove('active');\r\n//   langBtn.classList.remove('display-none');\r\n//   publishBtn.classList.remove('display-none');\r\n// }\r\n// const showBurgerMenu = () => {\r\n//   document.body.style.overflow = 'hidden';\r\n//   burgerMenu.classList.add('show');\r\n//   burgerBtn.classList.add('active');\r\n//   langBtn.classList.add('lang-btn--hidden');\r\n//   publishBtn.classList.add('display-none');\r\n// }\r\n\r\n// const toggleBurgerMenu = () => {\r\n//   if (burgerMenu.classList.contains('show')) {\r\n//     hideBurgerMenu()\r\n//   } else {\r\n//     showBurgerMenu()\r\n//   }\r\n// };\r\n\r\n// burgerBtn.addEventListener('click', toggleBurgerMenu);\r\n\n\n//# sourceURL=webpack://escn/./src/scripts/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The require scope
+/******/ 	var __webpack_require__ = {};
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/scripts/index.js"](0, __webpack_exports__, __webpack_require__);
+/******/ 	
+/******/ })()
+;
