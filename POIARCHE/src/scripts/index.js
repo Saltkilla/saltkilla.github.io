9244fadd31a71a3edd01a3df38141ec3dc0247a6
Denis Babin <markelizarov999@gmail.com>
2023-07-29 23:29:51 +0300

    added js

--- POIARCHE/src/scripts/index.js
+++ POIARCHE/src/scripts/index.js
@@ -1,31 +1,64 @@
-// // Burger Menu
+// Burger Menu
 
-// const burgerBtn = document.getElementById('burger-btn');
-// const burgerMenu = document.querySelector('.burger-menu');
-// const langBtn = document.querySelector('.header__lang');
-// const publishBtn = document.querySelector('.header__btn-publish');
+const burgerBtn = document.getElementById('burger-btn');
+const burgerMenu = document.querySelector('.burger-menu');
+const burgerWrraper = document.querySelector('.burger-wrraper');
+const burgerLinks = document.querySelectorAll('.burger-menu__link');
 
-// const hideBurgerMenu = () => {
-//   document.body.style.overflow = '';
-//   burgerMenu.classList.remove('show');
-//   burgerBtn.classList.remove('active');
-//   langBtn.classList.remove('display-none');
-//   publishBtn.classList.remove('display-none');
-// }
-// const showBurgerMenu = () => {
-//   document.body.style.overflow = 'hidden';
-//   burgerMenu.classList.add('show');
-//   burgerBtn.classList.add('active');
-//   langBtn.classList.add('lang-btn--hidden');
-//   publishBtn.classList.add('display-none');
-// }
+const hideBurgerMenu = () => {
+  document.body.style.overflow = '';
+  burgerWrraper.classList.remove('flex');
+  burgerMenu.classList.remove('show');
+  burgerBtn.classList.remove('active');
+}
+const showBurgerMenu = () => {
+  document.body.style.overflow = 'hidden';
+  burgerWrraper.classList.add('flex');
+  burgerMenu.classList.add('show');
+  burgerBtn.classList.add('active');
+}
 
-// const toggleBurgerMenu = () => {
-//   if (burgerMenu.classList.contains('show')) {
-//     hideBurgerMenu()
-//   } else {
-//     showBurgerMenu()
-//   }
-// };
+const toggleBurgerMenu = () => {
+  if (burgerMenu.classList.contains('show')) {
+    hideBurgerMenu()
+  } else {
+    showBurgerMenu()
+  }
+};
 
-// burgerBtn.addEventListener('click', toggleBurgerMenu);
+burgerBtn.addEventListener('click', toggleBurgerMenu);
+
+burgerLinks.forEach(link => {
+  link.addEventListener('click', toggleBurgerMenu);
+})
+
+// modals
+
+const allOrderBtn = document.querySelectorAll('.order-btn');
+const allJobBtn = document.querySelectorAll('.job-btn');
+const orderModal = document.getElementById('modal-order');
+const jobModal = document.getElementById('modal-job');
+const allCloseModal = document.querySelectorAll('.modal-order__close');
+
+allOrderBtn.forEach(btn => {
+  btn.addEventListener('click', () => {
+    orderModal.classList.add('block');
+  })
+})
+
+allJobBtn.forEach(btn => {
+  btn.addEventListener('click', () => {
+    jobModal.classList.add('block');
+  })
+})
+
+allCloseModal.forEach(close => {
+  close.addEventListener('click', () => {
+    if(orderModal.classList.contains('block')) {
+      orderModal.classList.remove('block');
+    }
+    if(jobModal.classList.contains('block')) {
+      jobModal.classList.remove('block');
+    }
+  })
+})
