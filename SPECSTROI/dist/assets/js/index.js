75d32d66a678545a6fb655f83683acfcd8c8fa1f
Denis Babin <markelizarov999@gmail.com>
2023-08-22 15:38:29 +0300

    specstroi work 1.0

---
+++ SPECSTROI/dist/assets/js/index.js
@@ -0,0 +1,49 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/scripts/index.js":
+/*!******************************!*\
+  !*** ./src/scripts/index.js ***!
+  \******************************/
+/***/ ((__unused_webpack___webpack_module__, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n//  sliders\r\n\r\nconst gritterSlider = new Swiper('.gritter', {\r\n    navigation: {\r\n        nextEl: '.gritter-slider-next',\r\n        prevEl: '.gritter-slider-prev',\r\n      },\r\n    slidesPerView: 'auto',\r\n    slidesPerGroup: 1,\r\n    spaceBetween: 0,\r\n    simulateTouch: true,\r\n})\r\n\r\nconst concreteMixingSlider = new Swiper('.concrete-mixing-slider', {\r\n    navigation: {\r\n        nextEl: '.concrete-mixing-slider-next',\r\n        prevEl: '.concrete-mixing-slider-prev',\r\n      },\r\n    slidesPerView: 'auto',\r\n    slidesPerGroup: 1,\r\n    spaceBetween: 0,\r\n    simulateTouch: true,\r\n})\r\n\r\nconst mountedExcavatorSlider = new Swiper('.mounted-excavator-slider', {\r\n    navigation: {\r\n        nextEl: '.mounted-excavator-slider-next',\r\n        prevEl: '.mounted-excavator-slider-prev',\r\n      },\r\n    slidesPerView: 'auto',\r\n    slidesPerGroup: 1,\r\n    spaceBetween: 0,\r\n    simulateTouch: true,\r\n})\r\n\r\nconst roadMillingSlider = new Swiper('.road-milling-slider', {\r\n    navigation: {\r\n        nextEl: '.road-milling-slider-next',\r\n        prevEl: '.road-milling-slider-prev',\r\n      },\r\n    slidesPerView: 'auto',\r\n    slidesPerGroup: 1,\r\n    spaceBetween: 0,\r\n    simulateTouch: true,\r\n})\r\n\r\nconst snowplowSlider = new Swiper('.snowplow-slider', {\r\n    navigation: {\r\n        nextEl: '.snowplow-slider-next',\r\n        prevEl: '.snowplow-slider-prev',\r\n      },\r\n    slidesPerView: 'auto',\r\n    slidesPerGroup: 1,\r\n    spaceBetween: 0,\r\n    simulateTouch: true,\r\n})\r\n\r\nconst largeBucketSlider = new Swiper('.large-bucket-slider', {\r\n    navigation: {\r\n        nextEl: '.large-bucket-slider-next',\r\n        prevEl: '.large-bucket-slider-prev',\r\n      },\r\n    slidesPerView: 'auto',\r\n    slidesPerGroup: 1,\r\n    spaceBetween: 0,\r\n    simulateTouch: true,\r\n})\r\n\r\nconst mainBucketSlider = new Swiper('.main-bucket-slider', {\r\n    navigation: {\r\n        nextEl: '.main-bucket-slider-next',\r\n        prevEl: '.main-bucket-slider-prev',\r\n      },\r\n    slidesPerView: 'auto',\r\n    slidesPerGroup: 1,\r\n    spaceBetween: 0,\r\n    simulateTouch: true,\r\n})\r\n\r\nconst dumpsSlider = new Swiper('.dumps-slider', {\r\n    navigation: {\r\n        nextEl: '.dumps-slider-next',\r\n        prevEl: '.dumps-slider-prev',\r\n      },\r\n    slidesPerView: 'auto',\r\n    slidesPerGroup: 1,\r\n    spaceBetween: 0,\r\n    simulateTouch: true,\r\n})\r\n\r\nconst palletForksSlider = new Swiper('.pallet-forks-slider', {\r\n    navigation: {\r\n        nextEl: '.pallet-forks-slider-next',\r\n        prevEl: '.pallet-forks-slider-prev',\r\n      },\r\n    slidesPerView: 'auto',\r\n    slidesPerGroup: 1,\r\n    spaceBetween: 0,\r\n    simulateTouch: true,\r\n})\r\n\r\nconst brushesSlider = new Swiper('.brushes-slider', {\r\n    navigation: {\r\n        nextEl: '.brushes-slider-next',\r\n        prevEl: '.brushes-slider-prev',\r\n      },\r\n    slidesPerView: 'auto',\r\n    slidesPerGroup: 1,\r\n    spaceBetween: 0,\r\n    simulateTouch: true,\r\n})\r\n\r\nconst jawBucketSlider = new Swiper('.jaw-bucket-slider', {\r\n    navigation: {\r\n        nextEl: '.jaw-bucket-slider-next',\r\n        prevEl: '.jaw-bucket-slider-prev',\r\n      },\r\n    slidesPerView: 'auto',\r\n    slidesPerGroup: 1,\r\n    spaceBetween: 0,\r\n    simulateTouch: true,\r\n})\r\n\r\nconst brushesWithHopperSlider = new Swiper('.brushes-with-hopper-slider', {\r\n    navigation: {\r\n        nextEl: '.brushes-with-hopper-slider-next',\r\n        prevEl: '.brushes-with-hopper-slider-prev',\r\n      },\r\n    slidesPerView: 'auto',\r\n    slidesPerGroup: 1,\r\n    spaceBetween: 0,\r\n    simulateTouch: true,\r\n})\r\n\r\nconst screeningBucketSlider = new Swiper('.screening-bucket-slider', {\r\n    navigation: {\r\n        nextEl: '.screening-bucket-slider-next',\r\n        prevEl: '.screening-bucket-slider-prev',\r\n      },\r\n    slidesPerView: 'auto',\r\n    slidesPerGroup: 1,\r\n    spaceBetween: 0,\r\n    simulateTouch: true,\r\n})\r\n\r\nconst clampBucketSlider = new Swiper('.clamp-bucket-slider', {\r\n    navigation: {\r\n        nextEl: '.clamp-bucket-slider-next',\r\n        prevEl: '.clamp-bucket-slider-prev',\r\n      },\r\n    slidesPerView: 'auto',\r\n    slidesPerGroup: 1,\r\n    spaceBetween: 0,\r\n    simulateTouch: true,\r\n})\r\n\r\nconst logGrappleSlider = new Swiper('.log-grapple-slider', {\r\n    navigation: {\r\n        nextEl: '.log-grapple-slider-next',\r\n        prevEl: '.log-grapple-slider-prev',\r\n      },\r\n    slidesPerView: 'auto',\r\n    slidesPerGroup: 1,\r\n    spaceBetween: 0,\r\n    simulateTouch: true,\r\n})\r\n\r\nconst foldingForksSlider = new Swiper('.folding-forks-slider', {\r\n    navigation: {\r\n        nextEl: '.folding-forks-slider-next',\r\n        prevEl: '.folding-forks-slider-prev',\r\n      },\r\n    slidesPerView: 'auto',\r\n    slidesPerGroup: 1,\r\n    spaceBetween: 0,\r\n    simulateTouch: true,\r\n})\r\n\r\nconst rearBucketSlider = new Swiper('.rear-bucket-slider', {\r\n    navigation: {\r\n        nextEl: '.rear-bucket-slider-next',\r\n        prevEl: '.rear-bucket-slider-prev',\r\n      },\r\n    slidesPerView: 'auto',\r\n    slidesPerGroup: 1,\r\n    spaceBetween: 0,\r\n    simulateTouch: true,\r\n})\r\n\r\nconst profileBucketSlider = new Swiper('.profile-bucket-slider', {\r\n    navigation: {\r\n        nextEl: '.profile-bucket-slider-next',\r\n        prevEl: '.profile-bucket-slider-prev',\r\n      },\r\n    slidesPerView: 'auto',\r\n    slidesPerGroup: 1,\r\n    spaceBetween: 0,\r\n    simulateTouch: true,\r\n})\r\n\r\nconst levelingBucketSlider = new Swiper('.leveling-bucket-slider', {\r\n    navigation: {\r\n        nextEl: '.leveling-bucket-slider-next',\r\n        prevEl: '.leveling-bucket-slider-prev',\r\n      },\r\n    slidesPerView: 'auto',\r\n    slidesPerGroup: 1,\r\n    spaceBetween: 0,\r\n    simulateTouch: true,\r\n})\r\n\r\nconst hydrohammerST5Slider = new Swiper('.hydrohammer-ST-5-slider', {\r\n    navigation: {\r\n        nextEl: '.hydrohammer-ST-5-slider-next',\r\n        prevEl: '.hydrohammer-ST-5-slider-prev',\r\n      },\r\n    slidesPerView: 'auto',\r\n    slidesPerGroup: 1,\r\n    spaceBetween: 24,\r\n    simulateTouch: true,\r\n})\r\n\r\n\r\n\r\n\r\n\n\n//# sourceURL=webpack://escn/./src/scripts/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The require scope
+/******/ 	var __webpack_require__ = {};
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/scripts/index.js"](0, __webpack_exports__, __webpack_require__);
+/******/ 	
+/******/ })()
+;
